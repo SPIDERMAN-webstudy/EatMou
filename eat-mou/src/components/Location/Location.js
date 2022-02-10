1aeba538ff996bb96568ff798a13fe22026f26e8
SeoDongSu <poopoo0515@naver.com>
2022-02-10 18:55:15 +0900

    test

---
+++ eat-mou/src/components/Location/Location.js
@@ -0,0 +1,21 @@
+import { useState } from "react";
+
+const location = (props) => {
+  const selectDistanceList = [100, 300, 500];
+  const dummyData = ["은혜식당", "미키식당", "백반기사집"];
+  const [distance, setDistance] = useState("1000");
+  const selectDistance = (event) => {
+    setDistance(event.target.value);
+  };
+
+  return (
+    <div>
+      <h2>서울시 광진구 광나루토 17길 52</h2>
+      <select onChange={selectDistance}>
+        <option value="100">100</option>
+        <option value="300">300</option>
+        <option value="500">500</option>
+      </select>
+    </div>
+  );
+};
