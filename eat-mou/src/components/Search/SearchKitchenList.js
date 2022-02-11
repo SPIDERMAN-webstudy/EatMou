a8d60c719f1f83be6f5f1491724b5ca262da2d58
Seugnjun <minsj47@gmail.com>
2022-02-12 02:51:25 +0900

    [ADD] SearchKitchenList 기능 추가

--- eat-mou/src/components/Search/SearchKitchenList.js
+++ eat-mou/src/components/Search/SearchKitchenList.js
@@ -2,21 +2,21 @@ import React from "react";
 const SearchKitchenList = (props) => {
   return (
     <React.Fragment>
-      {/* <div>{props.img}</div> */}
-      {console.log("hi")}
+      <div>{props.img}</div>
       <div>
         <h3>{props.name}</h3>
         <span>🦴 {props.dangol}</span>
-        <p>
+        <span>
           {props.today.map((value) => (
             <div>{value}</div>
           ))}
-        </p>
+        </span>
         <span>
           {props.openTime}~{props.closeTime}
         </span>
         <span>{props.address}</span>
         <span>{props.telephone}</span>
+        <h6>{props.distance}</h6>
       </div>
     </React.Fragment>
   );
