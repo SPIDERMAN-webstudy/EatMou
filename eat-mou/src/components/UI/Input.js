464cc1109b6e4e7c9b9dcd6e3225b72d02952df6
Seugnjun <minsj47@gmail.com>
2022-02-12 02:52:08 +0900

    [FIX] value 추가

--- eat-mou/src/components/UI/Input.js
+++ eat-mou/src/components/UI/Input.js
@@ -3,6 +3,7 @@ const Input = (props) => {
   return (
     <input
       placeholder={props.placeholder}
+      value={props.value}
       onChange={props.onChange}
       className={`${styles.input} ${props.className}`}
     />
