25bb72f02b0b842e4d75ffcecec04996cc204702
dongsuseo <poopoo0515@naver.com>
2022-02-11 18:27:18 +0900

    [FIX] KitchenList 수정

--- eat-mou/src/components/Location/LocationKitchenList.js
+++ eat-mou/src/components/Location/LocationKitchenList.js
@@ -1,38 +1,45 @@
-import {useState} from "react";
+import { useState, useEffect } from "react";
 
 import Card from "../UI/Card";
 
 const LocationKitchenList = (props) => {
-    const [isShort, setIsShort] = useState(false);
+  const [isShort, setIsShort] = useState(false);
 
-    if(props.distance<props.Sdistance){
-        setIsShort(true);
+  useEffect(() => {
+    if (props.distance < props.selectDistance) {
+      setIsShort(true);
+      console.log(isShort);
     }
-    
-    return (
-      <div>
-        {isShort ? (
-          <Card>
-            <img src={props.src} alt="" />
-            <div>
-              <h3>{props.name}</h3>
-              {props.count}
-            </div>
-            <div>{props.today}</div>
-            <div>
-              {`${props.openTime}~${props.closeTime}`}
-              {props.distance}
-            </div>
-            <div>
-              {props.address}
-              {props.telephone}
-            </div>
-          </Card>
-        ) : (
-          <div></div>
-        )}
-      </div>
-    );
-}
+    else{
+        setIsShort(false);
+    }
+    console.log(isShort);
+  }, [props.selectDistance]);
+
+  return (
+    <div>
+      {isShort ? (
+        <Card>
+          <img src={props.src} alt="" />
+          <div>
+            <h3>{props.name}</h3>
+            {props.count}
+          </div>
+          <div>{props.today}</div>
+          <div>
+            {`${props.openTime}~${props.closeTime}`}
+            {props.distance}
+          </div>
+          <div>
+            {props.address}
+            {props.telephone}
+          </div>
+        </Card>
+      ) : (
+        <div></div>
+      )}
+    </div>
+  );
+};
 
 export default LocationKitchenList;
