ee25d581d33e64eea00239146cce7414b3d71e8a
sooyoung <itsurboiace00@naver.com>
2022-02-13 17:56:03 +0900

    [FIX] 뼈다귀 클릭시 모양 바뀌게 수정

--- eat-mou/src/components/Info/Title.js
+++ eat-mou/src/components/Info/Title.js
@@ -1,16 +1,35 @@
 import { useSelector } from "react-redux";
+import { useState } from "react";
 
 import styles from "./Title.module.css";
-import Button from "../UI/Button";
 
 const Title = (props) => {
+  const [active, setActive] = useState(true);
   const kitchenData = useSelector((state) => state);
   const dong = kitchenData[0];
+
+  const dangolchangeHandler = () => {
+    setActive(false);
+  };
+
+  const dangolcancelHandler = () => {
+    setActive(true);
+  };
+
   return (
     <div>
       <span className={styles.name}>{dong.name}</span>
       <span className={styles.dangol}>{dong.dangol}</span>
-      <Button>🦴</Button>
+      {active && (
+        <h2 onClick={dangolchangeHandler} className={styles.bone}>
+          🦴
+        </h2>
+      )}
+      {!active && (
+        <h2 onClick={dangolcancelHandler} className={styles.bone}>
+          🍖
+        </h2>
+      )}
       <span className={styles.time}>
         {dong.openTime}~{dong.closeTime}
       </span>
