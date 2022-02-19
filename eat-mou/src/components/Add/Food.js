065a46fe18ab0aa0cf6b5d318206485260970c17
SeoDongSu <poopoo0515@naver.com>
2022-02-19 16:23:32 +0900

    [ADD] 메뉴 수정기능 추가

--- eat-mou/src/components/Add/Food.js
+++ eat-mou/src/components/Add/Food.js
@@ -1,12 +1,54 @@
-import React from "react";
+import React, {useState, useRef, useEffect} from "react";
 import PropTypes from "prop-types";
 
 const Food = ({today, todayList, setTodayList}) => {
-  console.log(today.text);
+  const [edited, setEdited] = useState(false);
+  const [newText, setNewText] = useState(today.text);
+
+  const editInputRef = useRef(null)
+
+  //------------------------------edit 모드 일때 포커싱---------------------------------------------
+  useEffect(()=>{
+    if(edited){
+      editInputRef.current.focus();
+    }
+  }, [edited]);
+//----------------------------------------------------------------------------------------------
+  const onClickEdit = () =>{
+    setEdited(true);
+  }
+  const onChangeEditInput= (event)=>{
+    setNewText(event.target.value);
+  }
+//---------------------수정된 아이템 id와 같은 것을 찾아 다시 넣어줌-------------------------------
+  const onClickSubmitButton = () =>{
+    const newTodayList = todayList.map((todayFood)=>(
+      {
+        ...todayFood,
+        text: todayFood.id === today.id ? newText : todayFood.text,
+      }
+    ));
+    setTodayList(newTodayList);
+
+    setEdited(false);
+  }
+  //-------------------------------------------------------------------------------------------------
   return (
     <li>
-      <span>{today.text}</span>
-      <button type="button">수정</button>
+      {edited ? <input type="text"
+      value={newText}
+      ref={editInputRef}
+      onChange={onChangeEditInput}
+      /> : <span>{today.text}</span>}
+      {edited ? (
+        <button type="button" onClick={onClickSubmitButton}>
+          확인
+        </button>
+      ) : (
+        <button type="button" onClick={onClickEdit}>
+          수정
+        </button>
+      )}
       <button type="button">삭제</button>
     </li>
   );
